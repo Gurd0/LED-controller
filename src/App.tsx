82f02c5322d6fbffd514c005c3a4bbf713f4a563
Gurd <gaf1902@outlook.com>
2024-09-24 21:41:15 +0200

    added analytic's

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 /** @format */
 
 import LEDControl from "./pages/index";
-
+import { Analytics } from "@vercel/analytics/react";
 function App() {
   return (
     <>
       <LEDControl />
+      <Analytics />
     </>
   );
 }
