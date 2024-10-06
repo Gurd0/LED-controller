02bd94b38946e4d4cb6755372d893ce9fb936696
Gurd <gaf1902@outlook.com>
2024-10-06 13:52:27 +0200

    added pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import tailwindcss from "tailwindcss";
+import { VitePWA } from 'vite-plugin-pwa'
  
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA()
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss()],
